6dec80ff053c4026c7d53e193c9733bd65a9df93
Yiduan <yiduanzh.usc@gmail.com>
2012-11-27 09:15:02 -0600

    second commit

---
+++ app/controllers/say_hello_controller.rb
@@ -0,0 +1,2 @@
+class SayHelloController < ApplicationController
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = '6da869cf3e1085c2fe2554a23ef1847eb7cda62ee574a2df1686be0414324502f94259b55955bfd6a65b3905da52e97e3146c03d69d951f14d5c588e3575c4d3'
